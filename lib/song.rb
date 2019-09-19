39654e7a3df093867b9d89b340e286148d97eeb4
Alex Farmer <farmale10@gmail.com>
2019-09-19 12:35:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,8 +47,10 @@ class Song
    end 
    
    def self.new_from_filename(file_name)
-     
-     
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
    end 
    
 
