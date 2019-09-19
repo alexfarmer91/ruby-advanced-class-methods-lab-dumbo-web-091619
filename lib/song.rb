acae74957c902611d08035f85824d5aa5388fbec
Alex Farmer <farmale10@gmail.com>
2019-09-19 12:31:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,13 +34,9 @@ class Song
  end 
 
 
- def self.find_or_create_by_name(name)
-   if self.find_by_name(name) != nil 
-     self.find_by_name(name)
-   else 
-     self.create_by_name(name)
+   def self.find_or_create_by_name(name)
+     self.find_by_name(name) || self.create_by_name(name)
    end 
- end 
    
    def self.alphabetical
      @@all.sort_by{|a| a.name}
